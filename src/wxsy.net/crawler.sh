5ed1beb1ad6deb786856338be4b904ccc88d6db8
Dnomd343 <i@343.re>
2022-10-15 03:12:28 +0800

    feat: crawler script

---
+++ src/wxsy.net/crawler.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+mkdir -p ./data/html/
+mkdir -p ./data/json/
+
+python3 catalog.py > ./data/catalog.json
+python3 fetch.py ./data/catalog.json ./data/html
+python3 extract.py ./data/catalog.json ./data/html ./data/json
+python3 release.py ./data/catalog.json ./data/json > ./data/xxrs.json

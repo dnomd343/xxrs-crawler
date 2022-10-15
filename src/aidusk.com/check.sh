a28c09fceb2ad9a5265d8b880049da9fbf6033b4
Dnomd343 <i@343.re>
2022-10-16 02:36:17 +0800

    remove: html hash check

--- src/aidusk.com/check.sh
+++ src/aidusk.com/check.sh
@@ -3,5 +3,4 @@
 cd `dirname $0`
 
 diff <(xz -cdk ./archive/catalog.json.xz | jq .) <(cat ./data/catalog.json | jq .)
-diff <(cd ./data/html/ && sha1sum * | sort -u) <(cat ./archive/html.sha1sum | sort -u)
 diff <(xz -cdk ./archive/xxrs.json.xz | jq .) <(cat ./data/xxrs.json | jq .)
--- src/wxsy.net/check.sh
+++ src/wxsy.net/check.sh
@@ -3,6 +3,5 @@
 cd `dirname $0`
 
 diff <(xz -cdk ./archive/catalog.json.xz | jq .) <(cat ./data/catalog.json | jq .)
-diff <(cd ./data/html/ && sha1sum * | sort -u) <(cat ./archive/html.sha1sum | sort -u)
 diff <(cd ./data/json/ && sha1sum * | sort -u) <(cat ./archive/json.sha1sum | sort -u)
 diff <(xz -cdk ./archive/xxrs.json.xz | jq .) <(cat ./data/xxrs.json | jq .)

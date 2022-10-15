ebba1da599965c4c27fdfa8d567bc86728de59ea
Dnomd343 <i@343.re>
2022-10-16 00:58:57 +0800

    update: check script

--- src/m.wxsy.net/crawler.sh
+++ src/m.wxsy.net/crawler.sh
@@ -5,6 +5,6 @@ mkdir -p ./data/html/
 mkdir -p ./data/json/
 
 python3 catalog.py > ./data/catalog.json
-python3 fetch.py ./data/catalog.json ./data/html
-python3 extract.py ./data/catalog.json ./data/html ./data/json
-python3 release.py ./data/catalog.json ./data/json > ./data/xxrs.json
+python3 fetch.py ./data/catalog.json ./data/html/
+python3 extract.py ./data/catalog.json ./data/html/ ./data/json
+python3 release.py ./data/catalog.json ./data/json/ > ./data/xxrs.json
--- src/wxsy.net/crawler.sh
+++ src/wxsy.net/crawler.sh
@@ -8,13 +8,3 @@ python3 catalog.py > ./data/catalog.json
 python3 fetch.py ./data/catalog.json ./data/html/
 python3 extract.py ./data/catalog.json ./data/html/ ./data/json
 python3 release.py ./data/catalog.json ./data/json/ > ./data/xxrs.json
-
-cd ./data/
-xz -k9 catalog.json
-tar cJf html.tar.xz html/
-tar cJf json.tar.xz json/
-xz -k9 xxrs.json
-
-mkdir -p ../archive/
-mv *.xz ../archive/
-cd ../
